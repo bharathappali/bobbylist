452538395d56996e4cc40a1779e61e8c939f7bc1
bharathappali <bharath.appali@gmail.com>
2017-11-09 09:31:41 +0530

    made chages with respect to mongo db

--- bobbylist.py
+++ bobbylist.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,10 +7,14 @@ app = Flask(__name__)
 def initiate_launch():
     return render_template("index.html")
 
-@app.route('/home')
-def launch_home():
-    return render_template("home.html")
-
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        email = request.form['signup_email']
+        password = request.form['signup_password']
+        return render_template("home.html")
 
 if __name__ == '__main__':
     app.run()
