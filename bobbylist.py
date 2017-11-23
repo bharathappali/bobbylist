463858e4037c9528ca1287d06392f5f54a55deb7
bharathappali <bharath.appali@gmail.com>
2017-11-23 13:07:10 +0530

    made invite people working

--- bobbylist.py
+++ bobbylist.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_triangle import Triangle
 from flask_mail import Mail, Message
 import hashlib
+
+from os.path import dirname, realpath
+from werkzeug.utils import secure_filename
+import os
 from pymongo import MongoClient
 from multiprocessing import Value
 
@@ -24,23 +28,25 @@ app.config.update(
 mongo_client = MongoClient('localhost',27017)
 bobbylistdb = mongo_client['bobbylist']
 
+UPLOAD_FOLDER = '/static/imgs/users'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 mail = Mail(app)
 
-def send_mail(recipients_list, activation_link):
+def send_mail(recipients_list,subject, message):
     msg = mail.send_message(
-        'Your activation link',
+        subject,
         sender='noreply.bobbylist@gmail.com',
         recipients=recipients_list,
-        body="Please click on this link http://127.0.0.1:5000/confirm/"+activation_link
+        body=message
     )
     return 'Mail sent'
 
 
 @app.route('/')
 def initiate_launch():
-    return render_template("dashboard.html")
+    return render_template("index.html")
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -49,7 +55,20 @@ def login():
     if request.method == 'POST':
         login_email = request.form['login_email']
         login_password = request.form['login_password']
-        return render_template("heyitsme.html")
+        if hashlib.sha256(login_password).hexdigest() == bobbylistdb.users.find_one({"email": login_email}, {"_id": 0})['hashpwd']:
+            user_db = bobbylistdb.users.find_one({"email": login_email}, {"_id": 0})
+            session['email'] = user_db['email']
+            session['img'] = user_db['img']
+            session['username'] = user_db['username']
+            tasks_start_db = bobbylistdb.tasks.find({'task_assignee': session['email'], "status": "start"}, {"_id": 0})
+            tasks_current_db = bobbylistdb.tasks.find({'task_assignee': session['email'], "status": "current"},
+                                                      {"_id": 0})
+            tasks_completed_db = bobbylistdb.tasks.find({'task_assignee': session['email'], "status": "completed"},
+                                                        {"_id": 0})
+            return render_template("dashboard.html", newuser=False, user_details=user_db,
+                                   tasks_start_details=tasks_start_db, tasks_current_details=tasks_current_db,
+                                   tasks_completed_details=tasks_completed_db)
+        return render_template("index.html")
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
@@ -67,7 +86,7 @@ def signup():
         verfication_hash_dict['checked'] = False
         recepients = []
         recepients.append(signup_email)
-        send_mail(recepients, hashemail)
+        send_mail(recepients,'Your activation link', "Please click on this link http://127.0.0.1:5000/confirm/"+hashemail)
         if bobbylistdb.email_verification.find_one({"hashemail":hashemail},{"_id":0}) == None:
             bobbylistdb.email_verification.insert(verfication_hash_dict)
         else:
@@ -80,44 +99,99 @@ def verifylink(verify_link):
     if user_verifilist['checked'] == False:
         return render_template("heyitsme.html", user_email= user_verifilist['email'])
     else:
-        user_db = bobbylistdb.users.find_one({"user_email":user_verifilist['email']},{"_id":0})
-        return render_template("dashboard.html", newuser=False, user_details = user_db)
+        # user_db = bobbylistdb.users.find_one({"user_email":user_verifilist['email']},{"_id":0})
+        return render_template("index.html")
 
 @app.route('/user_creation',  methods=['GET','POST'])
 def create_user():
     if request.method == 'GET':
         return render_template("index.html")
     if request.method == 'POST':
-        with counter.get_lock():
-            counter.value += 1
         signup_dict = {}
-        signup_dict['firstname_signup'] = request.form['firstnameinput']
-        signup_dict['lastname_signup'] = request.form['lastnameinput']
-        signup_dict['username_signup'] = request.form['usernameinput']
-        signup_dict['email_signup'] = request.form['emailinput']
-        user_verify_table_record = bobbylistdb.email_verification.find_one({"email":signup_dict['email_signup']},{"_id":0})
-        signup_dict['hashpwd_signup'] = user_verify_table_record['hashpwd']
-        signup_dict['checked_signup'] = user_verify_table_record['checked']
+        signup_dict['firstname'] = request.form['firstnameinput']
+        signup_dict['lastname'] = request.form['lastnameinput']
+        signup_dict['username'] = request.form['usernameinput']
+        signup_dict['email'] = request.form['emailinput']
+        people_list = []
+        signup_dict['people'] = people_list
+        user_verify_table_record = bobbylistdb.email_verification.find_one({"email":signup_dict['email']})
+        signup_dict['hashpwd'] = user_verify_table_record['hashpwd']
+        signup_dict['checked'] = True
+        if 'uploadfile' not in request.files:
+            signup_dict['img'] = './static/imgs/bobbylist_user.png'
+        else:
+            file = request.files['uploadfile']
+            if file.filename == "":
+                signup_dict['img'] = './static/imgs/bobbylist_user.png'
+            else:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(dirname(realpath(__file__))+app.config['UPLOAD_FOLDER'] , signup_dict['username'] + '.jpg'))
+                signup_dict['img'] = app.config['UPLOAD_FOLDER'] + '/' + signup_dict['username'] + '.jpg'
         bobbylistdb.users.insert(signup_dict)
-        user_db = bobbylistdb.users.find_one({"user_email": signup_dict['email_signup']}, {"_id": 0})
-        return render_template("dashboard.html", newuser=True, user_details = user_db)
+        bobbylistdb.email_verification.update({"_id": user_verify_table_record['_id']}, {"$set": {'checked': True}})
+        user_db = bobbylistdb.users.find_one({"email": signup_dict['email']}, {"_id": 0})
+        return render_template("dashboard.html", newuser=True, user_details = user_db, people_details= None)
 
 @app.route('/dashboard/create_task',  methods=['GET','POST'])
 def create_task():
     if request.method == 'GET':
         return render_template("index.html")
     if request.method == 'POST':
+        with counter.get_lock():
+            counter.value += 1
         task_dict = {}
+        task_dict['task_id'] = hashlib.sha256(str(counter.value)).hexdigest()
         task_dict['task_title'] = request.form['task_title']
         task_dict['task_assignee'] = request.form['task_assignee']
+        assigned_user = bobbylistdb.users.find_one({"email": task_dict['task_assignee']}, {"_id": 0})
+        task_dict['task_assignee_img'] = assigned_user['img']
+        task_dict['task_assignee_username'] = assigned_user['username']
         task_dict['task_assigned_by'] = request.form['task_assigned_by']
+        assigned_by_user = bobbylistdb.users.find_one({"email": task_dict['task_assigned_by']}, {"_id": 0})
+        task_dict['task_assigned_by_img'] = assigned_by_user['img']
+        task_dict['task_assigned_by_username'] = assigned_by_user['username']
         task_dict['task_desc'] = request.form['task_desc']
         task_dict['task_creation_date'] = request.form['task_creation_date']
         task_dict['task_due_date'] = request.form['task_due_date']
         task_dict['task_milestones'] = request.form.getlist('milestones')
+        task_dict['status'] = 'start'
+        task_dict['tag'] = ''
         bobbylistdb.tasks.insert(task_dict)
-        return "True"
+        user_db = bobbylistdb.users.find_one({"email": session['email']}, {"_id": 0})
+        people_db = user_db['people']
+        tasks_start_db = bobbylistdb.tasks.find({'task_assignee': session['email'], "status": "start"}, {"_id": 0})
+        tasks_current_db = bobbylistdb.tasks.find({'task_assignee': session['email'], "status": "current"}, {"_id": 0})
+        tasks_completed_db = bobbylistdb.tasks.find({'task_assignee': session['email'], "status": "completed"}, {"_id": 0})
+        return render_template("dashboard.html", newuser=False, people_list=people_db, user_details = user_db, tasks_start_details = tasks_start_db, tasks_current_details = tasks_current_db, tasks_completed_details = tasks_completed_db )
+
+@app.route('/dashboard/invite_people', methods=['GET', 'POST'])
+def invite_people():
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        added_people = request.data['add_people_input']
+        userdb = bobbylistdb.users.find_one({"email": session['email']})
+        people_list_user = userdb['people']
+        people_list_user.append(added_people)
+        bobbylistdb.users.update({"_id": userdb['_id']}, {"$set": {'people': people_list_user}})
+        receipients = []
+        receipients.append(added_people)
+        send_mail(receipients, session['email'] + ' has added you to his/her bobbylist contacts.', session['email']+' asks you to Join/Login http://www.bobbylist.com to view your pending tasks')
+        return ""
+
+@app.route('/dashboard/change_task_status', methods=['GET', 'POST'])
+def change_task_status():
+    if request.method == 'GET':
+        return render_template("index.html")
+    if request.method == 'POST':
+        task_id = request.json['task_id']
+        task_status = request.json['task_status']
+        bobbylistdb.tasks.update({"task_id": task_id}, {"$set": {'status': task_status}})
+        return True
+
 
 
 if __name__ == '__main__':
+    app.secret_key = 'tonystark'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run()
